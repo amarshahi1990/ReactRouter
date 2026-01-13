efcac28a71df58ba8b8b929e2a3440d9f777025a
amar.shahi1990 <shahi.shahi34@gmail.com>
2026-01-13 21:16:40 +0530

    app header

--- reactroteropeida/src/App.jsx
+++ reactroteropeida/src/App.jsx
@@ -4,7 +4,22 @@ import reactLogo from "./assets/react.svg";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div className="d-flex flex-column min-vh-100">
+        HEADER
+        <main className="container flex-grow-1 d-flex flex-column">
+          CONTENT
+        </main>
+        <footer
+          className="mt-auto py-3 bg-light border-top text-center text-muted"
+          style={{ width: "100%" }}
+        >
+          <p className="mb-0">@ 2026 React Router Demo.</p>
+        </footer>
+      </div>
+    </>
+  );
 }
 
 export default App;
